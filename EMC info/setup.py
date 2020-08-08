6caf6815fd20fe940ceee41d1bc13eb42fddaa34
joel <j.t.hooimeyer@gmail.com>
2020-08-08 16:30:24 +1000

    Added nations and residents Redid file structure

--- EMC info/setup.py
+++ EMC info/setup.py
@@ -1,22 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-classifiers = [
-    'Development Status :: Alpha',
-    'Intended Audience :: Gaming',
-    'Programming Language :: Python :: 3'
-]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='EMC info',
-    version='0.0.1',
-    description='A package to get info from the minecraft server EarthMC',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-    url='',
-    author='TheSuperGamer20578',
-    author_email='emc@thesupergamer20578.ml',
-    license='',
-    classifiers=classifiers,
-    keywords='',
-    packages=find_packages(),
-    install_requires=['']
+    name="EMC-info",
+    version="0.0.1",
+    description="",
+    py_modules=["EMC"],
+    package_dir={"": "src"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/TheSuperGamer20578/EMC-info/wiki",
+    author="TheSuperGamer20578",
+    author_email="emc@thesupergamer20578.ml"
 )
