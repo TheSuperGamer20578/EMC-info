a9d436a713de414431cf7f151879c9eb09b8e93b
joel <j.t.hooimeyer@gmail.com>
2020-09-06 10:50:08 +1000

    Added python 3.8 as supported lang

--- EMC-info/setup.py
+++ EMC-info/setup.py
@@ -15,6 +15,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
