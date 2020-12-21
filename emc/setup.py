0186f829636d14f95e898f1dbd15a6bd0faf266e
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2020-12-21 17:01:54 +1100

    renamed folder to make it easyer to import

---
+++ emc/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="EMC-info",
+    version="0.9",
+    description="EarthMC is a large Minecraft server this package lets you get info about things on that server.",
+    py_modules=["EMC"],
+    package_dir={"": "src"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    url="https://github.com/TheSuperGamer20578/EMC-info/wiki",
+    author="TheSuperGamer20578",
+    author_email="emc@thesupergamer20578.ml",
+    install_requires=[
+              "aiohttp",
+          ],
+)
