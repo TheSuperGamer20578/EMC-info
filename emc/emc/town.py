0ec319a89d205b09a1d6c73763cec762f190c8e9
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-01-14 13:16:08 +1100

    added town

--- emc/emc/town.py
+++ emc/emc/town.py
@@ -3,7 +3,7 @@ contains town class and town not found error
 """
 from typing import Tuple
 
-from . import util
+from . import util, resident, nation
 
 
 class TownNotFoundError(Exception):
@@ -18,16 +18,22 @@ class Town:
     """
 
     def __init__(self, name: str, *,
-                 data: Tuple[dict, dict] = util.get_data()):
+                 data: Tuple[dict, dict] = util.get_data(),
+                 _nation: nation.nation = None):
         if name not in data[0]:
             raise TownNotFoundError(
                 "The town {} could not be found".format(name))
         self.name = data[0][name]["label"]
+        if _nation is not None:
+            self.nation = _nation
+        else:
+            self.nation = nation.Nation(
+                data[0][name]["desc"][0][:-1].split("(")[-1], data=data)
         self.colour = data[0][name]["fillcolor"]
-        self.mayor = residents.Resident(data[0][name]["desc"][2], data=data,
-                                        town=self)
-        self.residents = [residents.Resident(resident, data=data, town=self)
-                          for resident in data[0][name]["desc"][4].split(", ")]
+        self.mayor = resident.Resident(data[0][name]["desc"][2], data=data,
+                                       _town=self)
+        self.residents = [resident.Resident(person, data=data, _town=self)
+                          for person in data[0][name]["desc"][4].split(", ")]
         self.flags = {
             "pvp": data[0][name]["desc"][7],
             "mobs": data[0][name]["desc"][8],
@@ -40,7 +46,11 @@ class Town:
         return self.name
 
     def __repr__(self) -> str:
-        return ("=== {} ===\ncolour: {}\nmayor: {}\nresidents: {}\n" +
-                "\n--- flags ---\npvp: {4['pvp']}\nmobs: {4['mobs']}\n" +
-                "explosions: {4['explosions']}\nfire: {4['fire']}\n" +
-                "capital: {4['capital']}")
+        return (
+                "=== {} ===\ncolour: {}\nmayor: {}\nresidents: {}\nnation: {}\n" +
+                "\n--- flags ---\npvp: {5['pvp']}\nmobs: {5['mobs']}\n" +
+                "explosions: {5['explosions']}\nfire: {5['fire']}\n" +
+                "capital: {5['capital']}"
+        ).format(self.name, self.colour, self.mayor.name,
+                 ", ".join([person.name for person in self.residents]),
+                 self.nation.name, self.flags)
