58ebbbd8d0779d3ebfceaebcc6171912e4a2d638
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-01-24 10:41:06 +1100

    resident stuff

--- emc/emc/resident.py
+++ emc/emc/resident.py
@@ -17,28 +17,37 @@ class Resident:
     """
     a person
     """
+
     def __init__(self, name: str, *, data: Tuple[dict, dict] = util.get_data(),
                  _town: town.Town = None):
-        minecraft = requests.get("https://api.mojang.com/users/profiles/minecraft/" + name)
+        minecraft = requests.get(
+            "https://api.mojang.com/users/profiles/minecraft/" + name)
         if minecraft.status_code != 200:
-            raise PlayerNotFoundError("The user {} was not found on Mojang's api or there was some other error: {}".format(name, minecraft.status_code))
-        res_data = next((person for person in data[1]["players"] if person["name"] == name), None)
+            raise PlayerNotFoundError(
+                "The user {} was not found on Mojang's api or there was some other error: {}".format(
+                    name, minecraft.status_code))
+        res_data = next((person for person in data[1]["players"] if
+                         person["name"] == name), None)
         self.name = name
         self.uuid = minecraft.json()["id"]
         if res_data is not None:
             self.online = True
             self.position = (res_data["x"], res_data["y"], res_data["z"])
-            if self.position == (0, 64, 0):
-                self.hidden = True
-            else:
-                self.hidden = False
+            self.hidden = True if self.position == (0, 64, 0) else False
+        self.town = town.Town([town_name for town_name in data[0] if
+                               name in data[0][town_name]["desc"][4]][0],
+                              data=data) or None
+        self.nation = self.town.nation or None
+        self.online = False
+        self.position = None
+        self.hidden = True
 
     def __str__(self) -> str:
         return self.name
 
     def __repr__(self) -> str:
         return (
-            "=== {} ===\nUUID: {}\nOnline: {}\nPosition: {3[0]}/{3[1]}/{3[2]}\n" +
-            "Hidden: {}".format(self.name, self.uuid, self.online, self.position,
-                                self.hidden)
-        )
+                "=== {} ===\nUUID: {}\nOnline: {}\nPosition: {3[0]}/{3[1]}/{3[2]}\n" +
+                "Hidden: {}\nTown: {}\nNation: {}"
+        ).format(self.name, self.uuid, self.online, self.position,
+                 self.hidden, self.town, self.nation)
