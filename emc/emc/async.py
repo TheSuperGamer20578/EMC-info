8cf73d04714dff53b872aedf56d687b5d763246d
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-03-06 12:39:54 +1100

    added async implementation of get_data

---
+++ emc/emc/async.py
@@ -0,0 +1,29 @@
+"""Async implementation of get_data"""
+from typing import Tuple
+from re import split
+
+from aiohttp import ClientSession
+
+HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
+
+URL_TOWN = "https://earthmc.net/map/tiles/_markers_/marker_earth.json"
+URL_PLAYER = "https://earthmc.net/map/up/world/earth/"
+
+
+async def get_data() -> Tuple[dict, dict]:
+    """Returns the map data"""
+    with ClientSession() as session:
+        async with session.get(URL_TOWN, headers=HEADERS) as resp_town, \
+                session.get(URL_PLAYER, headers=HEADERS) as resp_player:
+            if not resp_town.status == 200 == resp_player.status:
+                return await get_data
+            town_data = await resp_town.json()["sets"]["townyPlugin.markerset"]["areas"]
+            towns = {
+                name[:-3]: town[1] for name, town in zip(town_data, town_data.items()) if name.endswith("__0")
+            }
+            for town in towns:
+                towns[town]["desc"] = [desc for desc in split(r"<[^<>]*>", towns[town]["desc"]) if desc != ""]
+            return towns, await resp_player.json()
+
